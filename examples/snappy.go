4241b5116c71ba4a55d04840aa6eb38ca3fb6611
Tinkerer <tinkerer@zappem.net>
2025-04-02 06:37:22 -0700

    Add a zooming example
    Signed-off-by: Tinkerer <tinkerer@zappem.net>

--- examples/snappy.go
+++ examples/snappy.go
@@ -18,6 +18,7 @@ var (
 	config = flag.String("config", "snapmaker.config", "config file")
 	home   = flag.Bool("home", false, "home device (required after power on)")
 	photo  = flag.Bool("photo", false, "request a series of photos taken in a circle")
+	zoom   = flag.Bool("zoom", false, "request a series of zoomed photos")
 )
 
 type Config struct {
@@ -67,6 +68,20 @@ func main() {
 			r := 15.0
 			log.Printf("taking photo %d (at %.2f deg)", i, theta/math.Pi*180)
 			d, err := c.SnapJPEG(ctx, i, 192.5+r*math.Cos(theta), 170+r*math.Sin(theta), 170)
+			if err != nil {
+				log.Fatalf("photo grab failed: %v", err)
+			}
+			if err := os.WriteFile(fmt.Sprintf("photo%d.jpg", i), d, 0777); err != nil {
+				log.Fatalf("no photo: %v", err)
+			}
+		}
+	}
+
+	if *zoom {
+		for i := 0; i < 9; i++ {
+			delta := float64(i) * 3.0
+			log.Printf("taking photo %d (at %.2f mm)", i, 170-delta)
+			d, err := c.SnapJPEG(ctx, i, 192.5, 170, 170-delta)
 			if err != nil {
 				log.Fatalf("photo grab failed: %v", err)
 			}
